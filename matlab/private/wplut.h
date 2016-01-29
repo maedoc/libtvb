303f43a09659ce1e2003b15dad8d5facfb891da0
maedoc <marmaduke.woodman@univ-amu.fr>
2016-01-30 00:33:24 +0100

    move matlab bind dev to branch

--- matlab/private/wplut.h
+++
@@ -1,9 +0,0 @@
-/* copyright 2016 Apache 2 sddekit authors */
-
-/* defines wrapper pointer & look up table query */
-
-#include "matrix.h"
-
-typedef void (*wp)(int nl, mxArray **pl, int nr, mxArray **pr);
-
-wp sd_mex_lut(char *obj, char *meth);
--- matlab/sk_obj_meth.m
+++
@@ -1,6 +0,0 @@
-function h = sk_obj_meth(obj, meth)
-% sk_obj_meth Get function handle for object method.
-%
-% copyright 2016 Apache 2 sddekit authors
-
-h = eval(['@obj.' meth]);
