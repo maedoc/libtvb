ee104ec4e760edb28a4ccbe15c07c142423ca960
maedoc <marmaduke.woodman@univ-amu.fr>
2016-01-30 00:24:50 +0100

    clean up keep just c-api files

--- python/setup.py
+++ python/setup.py
@@ -2,44 +2,18 @@
 
 import os
 import glob
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
+from distutils.core import setup, Extension
 import numpy as np
 
 here = os.path.dirname(os.path.abspath(__file__))
-up = os.path.dirname(here)
+sources = glob.glob('src/*.c')
 
-"""
-sources = []
-for d, p in ((here, '*.pyx'), (up, 'src/*.c')):
-    sources += glob.glob(os.path.join(d, p))
+mod = Extension(
+        'sddekit', 
+        sources=['sddekit.c'] + sources,
+        include_dirs=['src', np.get_include()])
 
-exts = [Extension('hist',
-        sources=sources,
-        include_dirs=[
-            os.path.join(up, 'src'),
-            np.get_include()
-            ]
-        )
-        ]
-"""
-
-c_sources = glob.glob(os.path.join(up, 'src/*.c'))
-
-exts = []
-
-for pyx_file in glob.glob(os.path.join(here, '*.pyx')):
-    exts.append(Extension(pyx_file.split('.pyx')[0],
-        sources=[pyx_file] + c_sources,
-        include_dirs=[
-            os.path.join(up, 'src'),
-            np.get_include()
-            ]
-        ))
-
-
-setup(
-    name="sddekit",
-    ext_modules=cythonize(exts),
-)
+setup(name='SDDEKit',
+      version=0.0,
+      description='n/a',
+      ext_modules=[mod])
--- python/system.pxd
+++
@@ -1,5 +0,0 @@
-from sddekit_h cimport sd_sys
-
-cdef class Sys(object):
-    cdef sd_sys *_sys
-
