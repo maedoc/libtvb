ca69e6dc6348938af09b218bea684f455cea032d
maedoc <marmaduke.woodman@univ-amu.fr>
2016-01-29 20:35:14 +0100

    build ext mod per pyx file

--- python/setup.py
+++ python/setup.py
@@ -10,19 +10,36 @@ import numpy as np
 here = os.path.dirname(os.path.abspath(__file__))
 up = os.path.dirname(here)
 
+"""
 sources = []
 for d, p in ((here, '*.pyx'), (up, 'src/*.c')):
     sources += glob.glob(os.path.join(d, p))
 
-ext = Extension('hist',
+exts = [Extension('hist',
         sources=sources,
         include_dirs=[
             os.path.join(up, 'src'),
             np.get_include()
             ]
         )
+        ]
+"""
+
+c_sources = glob.glob(os.path.join(up, 'src/*.c'))
+
+exts = []
+
+for pyx_file in glob.glob(os.path.join(here, '*.pyx')):
+    exts.append(Extension(pyx_file.split('.pyx')[0],
+        sources=[pyx_file] + c_sources,
+        include_dirs=[
+            os.path.join(up, 'src'),
+            np.get_include()
+            ]
+        ))
+
 
 setup(
     name="sddekit",
-    ext_modules=cythonize([ext]),
+    ext_modules=cythonize(exts),
 )
