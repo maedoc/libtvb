8af5893a3f456a2388b382c3c168f360c346fae4
maedoc <marmaduke.woodman@univ-amu.fr>
2016-02-01 16:54:58 +0100

    add simple header

---
+++ src/sddekit_simple.h
@@ -0,0 +1,16 @@
+
+
+/**  SDDEKit, simplified header.
+ */
+
+/* included for bool, uint32_t & FILE types */
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+/* ignore these */
+#define SD_API
+#define restrict
+
+/* contains actual C declarations of API */
+#include "sddekit_api.h"
