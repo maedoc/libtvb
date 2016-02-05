7acbe244274d3bcb3b9c9fd530d9f9b9172bd39e
Abheek Ghosh <abheekghosh.ag@gmail.com>
2016-02-05 13:49:12 +0530

    Inital definition of functions for sys_regmap with dummy data

--- src/sys_regmap.c
+++ src/sys_regmap.c
@@ -3,6 +3,7 @@
 #include "sddekit.h"
 
 typedef struct regmapd {
+	//TODO, variables should be more or not, have to understand
 	sd_sys sys;
 	uint32_t i, *n;  /* n=1 for sum instead of averaging */
 } regmapd;
@@ -20,8 +21,53 @@ static sd_stat apply(sd_sys *sys, sd_sys_in *in, sd_sys_out *out)
 	return SD_OK;
 }
 
+static void sys_free(sd_sys *sys) {
+	sd_free(sys->ptr); 
+}
+
+static	uint32_t sys_ndim(sd_sys *sys) {
+	//TODO 
+	//return ((regmapd*) sys->ptr)->ndim;
+	//regmap dosen't have the required variables, have to understand what are we/regmap doing, so dummy return
+	return 0;
+}
+
+static	uint32_t sys_ndc(sd_sys *sys)  {
+	//TODO
+	//return ((regmapd*) sys->ptr)->ndc;
+	//regmap dosen't have the required variables, have to understand what are we/regmap doing, so dummy return
+	return 0;
+}
+
+static	uint32_t sys_nobs(sd_sys *sys) {
+	//TODO
+	//return ((regmapd*) sys->ptr)->obs;
+	//regmap dosen't have the required variables, have to understand what are we/regmap doing, so dummy return
+	return 0;
+}
+
+static	uint32_t sys_nrpar(sd_sys *sys){
+	//TODO
+	//return ((regmapd*) sys->ptr)->nrpar;
+	//regmap dosen't have the required variables, have to understand what are we/regmap doing, so dummy return
+	return 0;
+}
+
+static	uint32_t sys_nipar(sd_sys *sys){
+	//TODO
+	//This seems to work, but have to understand what are we/regmap doing
+	return ((regmapd*) sys->ptr)->i;
+}
+
 static sd_sys defaults = {
-	.apply = &apply
+	.ndim = &sys_ndim,
+	.ndc = &sys_ndc,
+	.nobs = &sys_nobs,
+	.nrpar = &sys_nrpar,
+	.nipar = &sys_nipar,
+	.apply = &apply,
+	.free = &sys_free,
+	.ptr = NULL
 };
 
 sd_sys *
