3ccf155be9082c249e4ccabc11093e67937db52e
maedoc <marmaduke.woodman@univ-amu.fr>
2016-02-11 00:10:05 +0100

    pass test name(s) as args to tests

--- test/main.c
+++ test/main.c
@@ -11,7 +11,15 @@
 #include "windows.h"
 #endif
 
-int main() {
+static void run_all() {
+#define TEST_FOUND(name) \
+	name();
+#include "test_list.h"
+#undef TEST_FOUND
+}
+
+
+int main(int argc, char *argv[]) {
 
 #ifdef _MSC_VER
 	char buf[1024];
@@ -19,10 +27,17 @@ int main() {
 	sd_log_info("cwd is %s\n", buf);
 #endif
 
-#define TEST_FOUND(name) \
-	name();
+	if (argc > 1) {
+		int i;
+		for (i=1; i<argc; i++) {
+#define TEST_FOUND(name) if (!strcmp(#name, argv[i])) name();
 #include "test_list.h"
 #undef TEST_FOUND
+		}
+	}
+	else
+		run_all();
+
 
 	sd_log_set_verbose(1);
 	return sd_test_report();
